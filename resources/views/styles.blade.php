226b61cc4725ba3f4b2489129f2fa4dc8ecf8002
core2drew <calupesandro@gmail.com>
2019-04-21 22:40:50 +0800

    add version for cache busting

--- resources/views/styles.blade.php
+++ resources/views/styles.blade.php
@@ -2,6 +2,6 @@
 <link href="{{ asset('fonts/rubik/style.css') }}" rel="stylesheet">
 <link href="{{ asset('fonts/cvsu-font-icon/style.css') }}" rel="stylesheet">
 <!-- Style -->
-<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+<link href="{{ mix('css/app.css') }}" rel="stylesheet">
 
-<link href="{{ asset('css/'.$module.'.css') }}" rel="stylesheet">
+<link href="{{ mix('css/'.$module.'.css') }}" rel="stylesheet">
