3b51abeed4f4e042b6d2798a89fcdb5f8d30c2de
core2drew <calupesandro@gmail.com>
2019-08-24 16:31:01 +0800

    add jquery cdn

--- resources/views/templates/scripts.blade.php
+++ resources/views/templates/scripts.blade.php
@@ -3,7 +3,7 @@
 @endif
 
 @if($module === 'site')
-<!-- <script src="https://code.jquery.com/jquery-3.4.0.min.js" integrity="sha256-BJeo0qm959uMBGb65z40ejJYGSgR7REI4+CW1fNKwOg=" crossorigin="anonymous"></script> -->
+<script src="https://code.jquery.com/jquery-3.4.0.min.js" integrity="sha256-BJeo0qm959uMBGb65z40ejJYGSgR7REI4+CW1fNKwOg=" crossorigin="anonymous"></script>
 <script src="{{ asset('vendors/owl-carousel/owl.carousel.min.js') }}" defer></script>
 <script src="{{ asset('js/facilities-slider.js') }}" defer></script>
 @endif
