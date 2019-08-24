b5acf8bf1d212726428d95004dad9e8bf3731d3c
core2drew <calupesandro@gmail.com>
2019-08-24 16:43:25 +0800

    show slider

--- resources/js/site-components/facility/index.js
+++ resources/js/site-components/facility/index.js
@@ -1,45 +1,46 @@
-import React, { useState } from 'react'
-import Modal from 'Components/modal'
-import './style.scss'
+import React, { useState } from "react";
+import Modal from "Components/modal";
+import "./style.scss";
 
 const Facility = props => {
-  const [isModalActive, setIsModalActive] = useState(false)
+    const [isModalActive, setIsModalActive] = useState(false);
 
-  const handleCloseModal = () => (
-    setIsModalActive(false)
-  )
-  
-  const handleOpenModal = () => (
-    setIsModalActive(true)
-  )
+    const handleCloseModal = () => setIsModalActive(false);
 
-  return (
-    <React.Fragment>
-      <Modal isActive={isModalActive} handleClose={handleCloseModal}>
-        <div className="details">
-          <div className="facility-slider owl-carousel owl-theme slider">
-            {
-              props.sliderImages.map((imageUrl, index) => (
-                <div className="image" style={{backgroundImage: `url(${imageUrl})`}} key={index}/>
-              ))
-            }
-          </div>
-          <p className="section title">{props.title}</p>
-          <p>{props.description}</p>
-        </div>
-      </Modal>
-      <div className="facility">
-        <div className="hover" onClick={handleOpenModal}>
-          View
-        </div>
-        <div className="feature-image" style={{backgroundImage: `url(${props.sliderImages[0]})`}} />
-        <div className="details">
-          <p className="title">{props.title}</p>
-          <p className="description">{props.description}</p>
-        </div>
-      </div>
-    </React.Fragment>
-  )
-}
+    const handleOpenModal = () => setIsModalActive(true);
 
-export default Facility
+    return (
+        <React.Fragment>
+            <Modal isActive={isModalActive} handleClose={handleCloseModal}>
+                <div className="details">
+                    <div className="facility-slider owl-carousel owl-theme slider">
+                        {props.sliderImages.map((imageUrl, index) => (
+                            <div
+                                className="image"
+                                style={{ backgroundImage: `url(${imageUrl})` }}
+                                key={index}
+                            />
+                        ))}
+                    </div>
+                    <p className="section title">{props.title}</p>
+                    <p>{props.description}</p>
+                </div>
+            </Modal>
+            <div className="facility">
+                <div className="hover" onClick={handleOpenModal}>
+                    View
+                </div>
+                <div
+                    className="feature-image"
+                    style={{ backgroundImage: `url(${props.sliderImages[0]})` }}
+                />
+                <div className="details">
+                    <p className="title">{props.title}</p>
+                    <p className="description">{props.description}</p>
+                </div>
+            </div>
+        </React.Fragment>
+    );
+};
+
+export default Facility;
