cc056d1e883126440d5b83043126f218d8b3993e
core2drew <calupesandro@gmail.com>
2019-10-01 21:50:04 +0800

    update main nav

--- resources/js/components/main-nav/index.js
+++ resources/js/components/main-nav/index.js
@@ -1,39 +1,48 @@
-import React from 'react'
-import Logo from 'Components/logo'
-import { Link, NavLink } from 'react-router-dom'
-import './style.scss'
+import React from "react";
+import Logo from "Components/logo";
+import { Link, NavLink } from "react-router-dom";
+import "./style.scss";
 
 const MainNav = () => (
-  <div id="MainNav">
-    <div className="container">
-      <Link to="/" className="sitename">
-        <Logo/>
-        <span className="schoolname">
-          <p className="name">Cavite State University</p>
-          <p className="branch">Cavite City Campus</p>
-        </span>
-      </Link>
-      <ul className="menu">
-        <NavLink className="item" to="/about">
-          About
-        </NavLink>
-        <NavLink className="item" to="/admission">
-          Admission
-          {/* <ul className="sub-menu">
-            <NavLink to="/admission#Hymn" className="item">Requirements</NavLink>
-            <NavLink to="/admission#RetentionPolicies" className="item">Retention Policies</NavLink>
-            <NavLink to="/admission#CourseOffered" className="item">Course Offered</NavLink>
-          </ul> */}
-        </NavLink>
-        <NavLink className="item" to="/facilities">
-          Facilities
-        </NavLink>
-        <NavLink className="item" to="/contact-us">
-          Contact Us
-        </NavLink>
-      </ul>
+    <div id="MainNav">
+        <div className="container">
+            <Link to="/" className="sitename">
+                <Logo />
+                <span className="schoolname">
+                    <p className="name">Cavite State University</p>
+                    <p className="branch">Cavite City Campus</p>
+                </span>
+            </Link>
+            <ul className="menu">
+                <NavLink className="item" to="/about">
+                    About
+                </NavLink>
+                <NavLink className="item" to="/admission">
+                    Admission
+                    <ul className="sub-menu">
+                        <NavLink to="/admission#Requirements" className="item">
+                            Requirements
+                        </NavLink>
+                        <NavLink
+                            to="/admission#RetentionPolicies"
+                            className="item"
+                        >
+                            Retention Policies
+                        </NavLink>
+                        <NavLink to="/admission#CourseOffered" className="item">
+                            Course Offered
+                        </NavLink>
+                    </ul>
+                </NavLink>
+                <NavLink className="item" to="/facilities">
+                    Facilities
+                </NavLink>
+                <NavLink className="item" to="/contact-us">
+                    Contact Us
+                </NavLink>
+            </ul>
+        </div>
     </div>
-  </div>
-)
+);
 
-export default MainNav
+export default MainNav;
