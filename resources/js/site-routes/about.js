e4de9c654cfb1c72921a502542b041b383b8eed5
core2drew <calupesandro@gmail.com>
2019-10-12 16:37:30 +0800

    update about layout

--- resources/js/site-routes/about.js
+++ resources/js/site-routes/about.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import History from 'SiteComponents/history';
-import MissionVision from 'SiteComponents/mission-vision';
-import Hymn from 'SiteComponents/hymn';
+import React from "react";
+import History from "SiteComponents/history";
+import MissionVision from "SiteComponents/mission-vision";
+import Hymn from "SiteComponents/hymn";
 
 const About = () => (
-  <div id="About">
-    <div className="container grid">
-      <History />
-      <MissionVision />
-      <Hymn />
+    <div id="About">
+        <div className="container grid">
+            <History />
+            <MissionVision />
+            <Hymn />
+        </div>
     </div>
-  </div>
-)
+);
 
-export default About
+export default About;
