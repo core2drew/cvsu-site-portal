35612af330cdb003c0fb31e8fa3d76a73cf3d3d3
core2drew <calupesandro@gmail.com>
2019-10-16 22:04:59 +0800

    add view more announcement button

--- resources/js/site-components/announcements/index.js
+++ resources/js/site-components/announcements/index.js
@@ -1,46 +1,45 @@
-import React, { useEffect, useState } from 'react'
-import Uuid from 'uuid/v4'
-import Button from 'Components/button'
-import { get } from 'Utils'
-import Announcement from './announcement'
-import './style.scss'
+import React, { useEffect, useState } from "react";
+import Uuid from "uuid/v4";
+import Button from "Components/button";
+import { get } from "Utils";
+import Announcement from "./announcement";
+import { NavLink } from "react-router-dom";
+import "./style.scss";
 
 const Announcements = () => {
-  const [announcements, setAnnouncements] = useState([])
-  const [nextPageURL, setNextPageURL] = useState(null)
+    const [announcements, setAnnouncements] = useState([]);
+    const [hasViewMore, setHasViewMore] = useState(false);
 
-  useEffect(() => {
-    get(
-      '/ajax/portal/limited/announcements', 
-      {},
-      res => {
-        setNextPageURL(res.next_page_url)
-        setAnnouncements(res.data)
-      }
-    )
-  }, [])
+    useEffect(() => {
+        get(
+            "/ajax/portal/limited/announcements",
+            {},
+            ({ data, next_page_url }) => {
+                if (next_page_url) {
+                    setHasViewMore(true);
+                }
+                setAnnouncements(data);
+            }
+        );
+    }, []);
 
-  const handleLoadMore = () => {
-    get(
-      nextPageURL, 
-      {},
-      res => {
-        setNextPageURL(res.next_page_url)
-        setAnnouncements([
-          ...announcements,
-          ...res.data
-        ])
-      }
-    )
-  }
+    return (
+        <div id="Announcements" className="section">
+            <p className="section header">Latest Announcements</p>
+            {announcements.map(announcement => (
+                <Announcement {...announcement} key={Uuid()} />
+            ))}
+            {hasViewMore && (
+                <NavLink
+                    to="/announcements"
+                    id="ViewMoreAnnouncements"
+                    className={"button -tertiary"}
+                >
+                    View More Announcements
+                </NavLink>
+            )}
+        </div>
+    );
+};
 
-  return (
-    <div id="Announcements" className="section">
-      <p className="section header">Latest Announcements</p>
-      { announcements.map(announcement => <Announcement {...announcement} key={Uuid()}/>) }
-      <Button id="LoadMoreAnnouncements" variant={'-tertiary'} text="Load More Announcements" onClick={handleLoadMore} isVisible={nextPageURL}/>
-    </div>
-  )
-}
-
-export default Announcements
+export default Announcements;
